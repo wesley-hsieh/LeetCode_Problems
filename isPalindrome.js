bf38f696fc4c5882c6a373506edbc2e1a527df6f
Wesley <whsieh1996@gmail.com>
2023-02-01 15:12:24 -0800

    initial main

---
+++ isPalindrome.js
@@ -0,0 +1,14 @@
+function isPalidrome(x){
+    let reverse = 0;
+    let copy = x;
+
+    while (copy > 0){
+        const digit = copy % 10;
+        reverse = reverse*10 + digit;
+        copy = Math.floor(copy/10);
+
+        //console.log(`digit ${digit} reverse ${reverse} copy ${copy}`);
+    }
+
+    return reverse === x;
+}
